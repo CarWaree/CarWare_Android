6bf5fd64f9f57f9c2b23c9f450bf80a0a17859c4
Zyadoo34 <zyadosama2756@gmail.com>
2025-10-28 15:14:25 +0300

    update signUp & login &reset pass

--- app/src/main/java/com/example/carware/MainActivity.kt
+++ app/src/main/java/com/example/carware/MainActivity.kt
@@ -9,21 +9,62 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Composition
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.carware.nav.HomeScreen
+import com.example.carware.nav.LoginScreen
+import com.example.carware.nav.NewPasswordScreen
+import com.example.carware.nav.Screens.HomeScreen
+import com.example.carware.nav.Screens.LoginScreen
+import com.example.carware.nav.ResetPasswordScreen
+import com.example.carware.nav.Screens.NewPasswordScreen
+import com.example.carware.nav.VerficationCodeScreen
+import com.example.carware.nav.Screens.ResetPasswordScreen
+import com.example.carware.nav.Screens.SignUpScreen
+import com.example.carware.nav.Screens.VerficationCodeScreen
+import com.example.carware.nav.SignUpScreen
 import com.example.carware.ui.theme.CarWareTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val navController = rememberNavController()
+
             CarWareTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    NavHost(navController=navController,
+                        startDestination = SignUpScreen,
+                        modifier = Modifier.padding(innerPadding))
+                        {
+                            composable<SignUpScreen> {
+                                SignUpScreen(navController)
+                            }
+                            composable<LoginScreen> {
+                                LoginScreen(navController)
+                            }
+                            composable<HomeScreen> {
+                                HomeScreen(navController)
+                            }
+                            composable<ResetPasswordScreen> {
+                                ResetPasswordScreen(navController)
+                            }
+                            composable<VerficationCodeScreen> {
+                                VerficationCodeScreen(navController)
+                            }
+                            composable<NewPasswordScreen> {
+                                NewPasswordScreen(navController)
+                            }
+
+
+
+                    }
                 }
             }
         }
